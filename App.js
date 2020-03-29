c20b7050d2e10a4407d26cbd07702a43fccc531f
Justin Singh <justin.singh125125@gmail.com>
2020-03-29 13:13:31 -0700

    boiler plate almost complete

--- App.js
+++ App.js
@@ -4,42 +4,101 @@ import { createAppContainer, createSwitchNavigator } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
 import { createBottomTabNavigator } from "react-navigation-tabs";
 
-
 //screens
-import IndexScreen from "./src/screens/index";
-import LoginScreen from "./src/screens/Login";
-import SignupScreen from "./src/screens/Signup";
+import IndexScreen from "./src/screens/Search/index";
+
+import SavedScreen from "./src/screens/Saved/Saved";
+
+import FollowersScreen from "./src/screens/Followers/Followers";
+
+import ProfileScreen from "./src/screens/Profile/Profile";
+
+
+//icons
+import { Feather, MaterialIcons } from "@expo/vector-icons";
+
+
+
 
 //context
 import { Provider as AuthProvider } from "./src/context/AuthContext";
 
-const switchNavigator = createSwitchNavigator({
-  loginFlow: createStackNavigator({
-    Signup: SignupScreen,
-    Login: LoginScreen
-  }),
-  mainFlow: createBottomTabNavigator({
-    CustomerFlow: createStackNavigator({
-      Search: IndexScreen
-    })
-  })
-});
+const defaultNavigationSettings = {
+  title: false,
+  headerTransparent: true,
+}
 
-const navigator = createStackNavigator(
-  {
-    Index: IndexScreen
+const tabNavigator = createBottomTabNavigator({
+  SearchFlow: createStackNavigator({
+    //screen section
+    Search: IndexScreen,
+
   },
-  {
-
-    initialRouteName: 'Index',
-    defaultNavigationOptions: {
-      title: false,
-      headerTransparent: true
+    //settings section
+    {
+      initialRouteName: 'Search',
+      defaultNavigationOptions: defaultNavigationSettings,
+      navigationOptions: {
+        tabBarLabel: 'Search',
+        tabBarIcon: ({ tintColor }) => (
+          <Feather name="search" color={tintColor} size={25} />
+        )
+      }
     },
-  }
-);
 
-const App = createAppContainer(navigator);
+  ),
+  SavedFlow: createStackNavigator({
+    Saved: SavedScreen
+  },
+    {
+      initialRouteName: 'Saved',
+      defaultNavigationOptions: defaultNavigationSettings,
+      navigationOptions: {
+        tabBarLabel: 'Search',
+        tabBarIcon: ({ tintColor }) => (
+          <MaterialIcons name="favorite-border" color={tintColor} size={25} />
+        )
+      }
+    },
+  ),
+  FollowersFlow: createStackNavigator({
+    Followers: FollowersScreen
+  },
+    {
+      initialRouteName: 'Followers',
+      defaultNavigationOptions: defaultNavigationSettings,
+      navigationOptions: {
+        tabBarLabel: 'Search',
+        tabBarIcon: ({ tintColor }) => (
+          <Feather name="link-2" color={tintColor} size={25} />
+        )
+      }
+    },
+  ),
+  ProfileFlow: createStackNavigator({
+    Profile: ProfileScreen
+  },
+    {
+      initialRouteName: 'Profile',
+      defaultNavigationOptions: defaultNavigationSettings,
+      navigationOptions: {
+        tabBarLabel: 'Search',
+        tabBarIcon: ({ tintColor }) => (
+          <Feather name="user" color={tintColor} size={25} />
+        )
+      }
+    },
+  )
+},
+  {
+    tabBarOptions: {
+      showIcon: true,
+      activeTintColor: '#e22222',
+    }
+  });
+
+
+const App = createAppContainer(tabNavigator);
 
 export default () => {
   return (
---
+++ src/screens/Followers/Followers.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import {
+    View,
+    Text,
+    StyleSheet,
+} from 'react-native';
+
+const FollowersScreen = props => {
+
+    return (
+
+        //give main view content margin top of the header
+        <View style={styles.container}>
+            <Text>Followers screen</Text>
+
+        </View >
+    );
+};
+
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        // we give view margin top because that is how
+        //big the transparent header is
+        marginTop: 88
+    }
+});
+
+export default FollowersScreen;
